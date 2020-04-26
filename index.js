6c0ac5991e913c9d342d83d72dd0b89c30e99314
mwturner611 <mwturner611@gmail.com>
2020-04-25 20:41:36 -0500

    fix query bug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var table = require("console-tabe");
+var table = require("console.table");
 const env = require("dotenv").config({path: '.env'});
-var query = require("query.js");
+var query = require("./query.js");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,10 +11,14 @@ var connection = mysql.createConnection({
 
     user: "root",
 
-    headers: `${process.env.GH_TOKEN}`,
+    password: `${process.env.SQLPW}`,
     database: "humanresources_DB"
 });
 
 connection.connect(function(err){
     if (err) throw err;
 })
+
+query.eeQuery(connection);
+query.deptQuery(connection);
+query.roleQuery(connection);
--- query.js
+++ query.js
@@ -1,29 +1,33 @@
-function eeQuery(employee){
+
+
+
+function eeQuery(connection){
     connection.query(
         "SELECT * FROM employee", function(err,results){
             if(err) throw err;
-        }
-        .then(function(answer){
-            console.table(answer);
-        }));
+
+            console.table(results)
+        });
 };
 
-function deptQuery(employee){
+function deptQuery(connection){
     connection.query(
         "SELECT * FROM department", function(err,results){
             if(err) throw err;
-        }
-        .then(function(answer){
-            console.table(answer);
-        }));
+
+            console.table(results);
+        });
 };
 
-function roleQuery(employee){
+function roleQuery(connection){
     connection.query(
         "SELECT * FROM role", function(err,results){
             if(err) throw err;
-        }
-        .then(function(answer){
-            console.table(answer);
-        }));
+
+            console.table(results);
+        });
 };
+
+module.exports.eeQuery = eeQuery;
+module.exports.deptQuery = deptQuery;
+module.exports.roleQuery = roleQuery;
