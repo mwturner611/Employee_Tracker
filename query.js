02585a9b4cd77ab8635e510c568fafbf1ea7b48b
mwturner611 <mwturner611@gmail.com>
2020-04-30 00:17:12 -0500

    Comments and clean up

--- query.js
+++ query.js
@@ -67,19 +67,22 @@ function rolesQuery(){
         });
 };
 
-//All Departments as a list for new role
+//User selects add a new role - this query gathers a consolidated list of departments and launches "newRole" function sending list
 function deptsList(){
     var depts = [];
 
     connection.query(
         "SELECT * FROM department", function(err,results){
             if(err) throw err;
+
+            // loop through departments creating a new string with "### deptName" so user has a list in inquirer
             for (var i = 0; i < results.length; i++){
                 var newid = results[i].id;
                 var newName = results[i].name;
                 var idName = newid + " " + newName;
                 depts.push(idName);
             }
+            // send department list to newrole function
             questions.newRole(depts);
             
         });
@@ -87,16 +90,20 @@ function deptsList(){
 
 // All EEs and titles as lists for EE transfer
 function eeList(){
+    // two epmty arrays
     var roles = [];
     var ees = [];
 
+    // query parameters
     var query1 = "SELECT id, title FROM role";
 
     var query2 = "SELECT id, last_name FROM employee";
 
+    // query connection
     connection.query(query1, function(err,results){
             if(err) throw err;
 
+            // loop through available titles and create "### title" for inquirer to have consolidated list for user
             for (var i = 0; i < results.length; i++){
                 var newID = results[i].id;
                 var newTitle = results[i].title;
@@ -104,16 +111,18 @@ function eeList(){
                 roles.push(idTitle);
             }
 
-            
+            // query2 connection
             connection.query(query2, function(err,results){
                     if(err) throw err;
         
+                    // loop through available employees and create "### name" for inquirer to have consolidated list for user
                     for (var i = 0; i < results.length; i++){
                         var id = results[i].id;
                         var last_name = results[i].last_name;
                         var idLname = id + " " + last_name;
                         ees.push(idLname);
                       }
+                    //   send the lists to transfer function
                       questions.transfer(roles,ees);
                 });
                 
@@ -135,6 +144,7 @@ function rolesList(){
     connection.query(query1, function(err,results){
             if(err) throw err;
 
+            // loop through ids and titles to create "### title" consolidated list for inquirer
             for (var i = 0; i < results.length; i++){
                 var newID = results[i].id;
                 var newTitle = results[i].title;
@@ -144,6 +154,7 @@ function rolesList(){
             connection.query(query2, function(err,results){
                 if(err) throw err;
                 
+                // loop through role IDs and names to create "### name" consolidated list for inquirer prompt
                 for (var i = 0; i < results.length; i++){
                   var mgrName = results[i].first_name;
                   var mgrID = results[i].role_id;
@@ -153,6 +164,7 @@ function rolesList(){
 
                 }
             })
+            // send the lists to newEmployee function
             questions.newEmployee(roles,mgrs);
 
             
@@ -162,6 +174,7 @@ function rolesList(){
 // add EE to database 
 function addEmployee(employee){
 
+    // with user's answers, add new employee to the database
    connection.query("INSERT INTO employee SET ?",
     {
        first_name: employee.first_name,
@@ -176,7 +189,7 @@ function addEmployee(employee){
   
 }
 
-// Add a role
+// with user's answers, add new role to the database
 function addRole(role){
 
     connection.query("INSERT INTO role SET ?",
@@ -192,7 +205,7 @@ function addRole(role){
     )
 }
 
-// Add a department
+// with user's answers, add new department to the database
 function addDepartment(newDept){
     connection.query("INSERT INTO department SET ?",
     {
@@ -205,7 +218,7 @@ function addDepartment(newDept){
     )
 };
 
-// Change an employee's role
+// with the user's selection of the employee's id and the selected role's id, make the transfer
 function updateEE(ee_id,role_id){
     connection.query("UPDATE employee SET ? WHERE ?",
     [
@@ -225,26 +238,7 @@ function updateEE(ee_id,role_id){
     )
 }
 
-// Specific role
-function roleQuery(role){
-    var query = "SELECT * FROM role WHERE ?";
-
-    connection.query(query, {title: role}, function(err, results) {
-        if(err){throw err}
-        else if(results.length === 0){
-            error();
-
-            questions.view();
-        }
-        else{
-        console.table(results);
-
-        questions.kickoff();
-        }} 
-    )
-};
-
-// Create an org chart with EE's manager/title/dept
+// Create an org chart with EE's manager/title/dept joining all tables and a self join to get manager
 function orgChartQuery (){
     // Select columns
     var query = "SELECT e1.id,e1.first_name,e1.last_name,e1.role_id,r1.title,r1.salary,r1.department_id,d1.name AS dept_name,CONCAT(e2.first_name,' ',e2.last_name) AS Mgr_Name,r2.title AS Mgr_Title ";
@@ -269,7 +263,7 @@ function orgChartQuery (){
         });
 }
 
-// Specific employee
+// Specific employee query - creating an indivual org chart
 function eeQuery(lastName){
     // Select columns
     var query = "SELECT e1.id,e1.first_name,e1.last_name,e1.role_id,r1.title,r1.salary,r1.department_id,d1.name AS dept_name,CONCAT(e2.first_name,' ',e2.last_name) AS Mgr_Name,r2.title AS Mgr_Title ";
@@ -311,7 +305,6 @@ module.exports.deptsQuery = deptsQuery;
 module.exports.rolesQuery = rolesQuery;
 module.exports.orgChartQuery = orgChartQuery;
 module.exports.addDepartment = addDepartment;
-module.exports.roleQuery = roleQuery;
 module.exports.rolesList = rolesList;
 module.exports.connection = connection;
 module.exports.addEmployee = addEmployee;
